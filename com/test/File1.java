5f4d173e78b68c1baa2e4a9fe7e277806d784117
ritickgautan <ritick.gautam.javaventures@gmail.com>
2022-02-24 09:21:53 -0500

    Added new branch and updated File1

--- com/test/File1.java
+++ com/test/File1.java
@@ -1,8 +1,13 @@
 package com.test;
 public class File1
 {
+	public void develop() {
+		System.out.println("Inside develop");
+	}
+
   public static void main(String ar[])
   {
+
     System.out.println("Hi from Ecllipse");
   }
 }
