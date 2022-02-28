aa93c193215479239eacf5d70d65db1ea9320c64
ritickgautan <ritick.gautam.javaventures@gmail.com>
2022-02-28 12:54:19 -0500

    Updated TestGit.java from Feature 2 branch

--- com/assignment/TestGit.java
+++ com/assignment/TestGit.java
@@ -2,7 +2,7 @@ package com.assignment;
 
 public class TestGit {
 	public static void main(String[] args) {
-		System.out.println("Hello feature from feature 1");
+		System.out.println("Hello feature from feature 2");
 	}
 
 }
